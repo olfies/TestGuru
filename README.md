42a6dc6ca48fd9d6ed3243fa9eef7fe91c20cc21
Gleb <glebonavt@bk.ru>
2023-05-17 23:30:50 +0300

    try 10

---
+++ README.md
@@ -0,0 +1,11 @@
+TestGuru
+
+TestGuru is testing system where users can create, manage and pass different types of tests.
+Project objects
+
+    Users
+    Roles
+    Categories
+    Test
+    Questions
+    Answers
---
+++ db/schema.rb
@@ -0,0 +1,83 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_05_17_200944) do
+
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "plpgsql"
+
+    create_table "answers", force: :cascade do |t|
+      t.text "body", null: false
+      t.boolean "correct", default: false, null: false
+      t.integer "question_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.index ["question_id"], name: "index_answers_on_question_id"
+    end
+
+    create_table "categories", force: :cascade do |t|
+      t.string "title", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
+
+    create_table "questions", force: :cascade do |t|
+      t.text "body", null: false
+      t.integer "test_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.index ["test_id"], name: "index_questions_on_test_id"
+    end
+
+    create_table "tests", force: :cascade do |t|
+      t.string "title", null: false
+      t.integer "level", default: 0, null: false
+      t.integer "category_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.boolean "published", default: false, null: false
+      t.index ["category_id"], name: "index_tests_on_category_id"
+      t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
+    end
+
+    create_table "users", force: :cascade do |t|
+      t.string "first_name", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer "sign_in_count", default: 0, null: false
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string "current_sign_in_ip"
+      t.string "last_sign_in_ip"
+      t.string "confirmation_token"
+      t.datetime "confirmed_at"
+      t.datetime "confirmation_sent_at"
+      t.string "unconfirmed_email"
+      t.string "type", default: "User", null: false
+      t.string "last_name"
+      t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+      t.index ["type"], name: "index_users_on_type"
+    end
+
+    add_foreign_key "answers", "questions"
+    add_foreign_key "feedbacks", "users"
+    add_foreign_key "questions", "tests"
+    add_foreign_key "tests", "categories"
+
+  end
