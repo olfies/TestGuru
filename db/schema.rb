8f474c444b65517a4d849fcce9bc496d3ff8e4a0
Gleb <glebonavt@bk.ru>
2023-05-19 22:49:31 +0300

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_200954) do
+ActiveRecord::Schema.define(version: 2023_05_17_200958) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2023_05_17_200954) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 0, null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -43,4 +52,5 @@ ActiveRecord::Schema.define(version: 2023_05_17_200954) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
