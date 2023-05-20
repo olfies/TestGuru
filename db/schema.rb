6631fd765a9c3134d5912eaca0ddc20672c362b3
Gleb <glebonavt@bk.ru>
2023-05-20 23:22:29 +0300

    fix 101

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_200958) do
+ActiveRecord::Schema.define(version: 2023_05_20_200958) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_05_17_200958) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
