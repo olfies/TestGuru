e1b18b83642cd6abf074e0f488d85ca4de4ba746
Gleb <glebonavt@bk.ru>
2023-05-24 17:43:22 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
-  def self.get_titles(category_name)
-    Test.joins(' JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_name)
-        .order(title: :desc).pluck(:title)
+  belongs_to :category
+  has_and_belongs_to_many :users
+
+  def self.sort_by_category(category_title)
+    joins(:category).where(categories: { title: category_title}).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  def tests_by_level(level)
-    Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
-        .where("tests.level = ?",level).where("user_tests.user_id = ?", id)
+  has_and_belongs_to_many :tests
+
+  def user_tests(test_level)
+    tests.where(level: test_level)
   end
 end
