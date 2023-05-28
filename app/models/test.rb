92485dbe0ad7dfcacd11bf2014121b947fcb9a85
Gleb <glebonavt@bk.ru>
2023-05-28 20:35:40 +0300

    lessons 5 associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :user_tests
+  has_many :users, through: :user_tests
+
   def self.get_titles(category_name)
     Test.joins(' JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_name)
         .order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+
   def tests_by_level(level)
     Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
         .where("tests.level = ?",level).where("user_tests.user_id = ?", id)
