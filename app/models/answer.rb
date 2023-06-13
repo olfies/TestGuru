f8e7a3f62ff40f83fcbbe5685f44fe191b85c94b
Gleb <glebonavt@bk.ru>
2023-06-13 20:15:14 +0300

    git scope and validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+  validate :max_answer_count
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def max_answer_count
+    errors.add("can't have more than 4 answers") if question.answers.size >= 4
+  end
 end
