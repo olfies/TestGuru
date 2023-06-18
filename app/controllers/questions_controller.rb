ad0f31b462114dc3ebccbf4d95429f95441423d4
Gleb <glebonavt@bk.ru>
2023-06-18 12:25:45 +0300

    rails controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,60 @@
+class QuestionsController < ApplicationController
+    before_action :find_question, only: %i[show destroy edit update]
+    before_action :find_test, only: %i[index create new edit destroy]
+
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+    def new
+      @question = @test.questions.new
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def index
+      @question = @test.questions
+    end
+
+    def create
+      @question = @test.question.new(question_params)
+      if @question.save
+        redirect_to @question
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @question.update(question_params)
+        redirect_to @question
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @question.destroy
+        redirect_to test_questions_path(@question.test_id)
+    end
+
+    private
+
+    def find_test
+        @test = Test.find(params[:test_id])
+      end
+
+      def find_question
+        @question = Question.find(params[:id])
+      end
+
+      def question_params
+        params.require(:question).permit(:body)
+      end
+
+      def rescue_with_question_not_found
+        render plain: "Question not found."
+      end
+    end
