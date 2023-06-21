f9721da95baa491f6a5fc3e7a34f3d9e2a66e1cf
Gleb <glebonavt@bk.ru>
2023-06-21 21:43:35 +0300

    add shared

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
-end
+    def github_url(author, repo)
+      link_to repo, "https://github.com/#{author}/#{repo}", target: :blank
+    end
+
+    def current_year
+      Date.current.year
+    end
+  end
