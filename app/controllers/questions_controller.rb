ebee9151e27ca1158c8f5267b7d384506ee1ab41
Gleb <glebonavt@bk.ru>
2023-07-04 03:44:58 +0300

    add+

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,11 +24,21 @@ class QuestionsController < ApplicationController
 
   private
 
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
     render plain: 'Question not found'
   end
+
 end
--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-    def github_url(author, repo)
-      link_to repo, "https://github.com/#{author}/#{repo}", target: :blank
-    end
-
-    def current_year
-      Date.current.year
-    end
-  end
