d93b477a41e31bb467b06561a578c298c4f7d4ea
Gleb <glebonavt@bk.ru>
2023-07-04 03:35:14 +0300

    try to fix problems

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,60 +1,34 @@
 class QuestionsController < ApplicationController
-    before_action :find_question, only: %i[show destroy edit update]
-    before_action :find_test, only: %i[index create new edit destroy]
+  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
 
-    rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-    def new
-      @question = @test.questions.new
-    end
+  def index
+    @question = @test.questions
+  end
 
-    def show
-    end
+  def show
+    render plain: @question.body
+  end
 
-    def edit
-    end
+  def create
+    new_question = @test.questions.new(question_params)
+    new_question.save
+    render plain: "New question was created: #{new_question.body}"
+  end
 
-    def index
-      @question = @test.questions
-    end
+  def destroy
+    @question.destroy
+  end
 
-    def create
-      @question = @test.question.new(question_params)
-      if @question.save
-        redirect_to @question
-      else
-        render :new
-      end
-    end
+  private
 
-    def update
-      if @question.update(question_params)
-        redirect_to @question
-      else
-        render :new
-      end
-    end
+  def find_question
+    @question = Question.find(params[:id])
+  end
 
-    def destroy
-      @question.destroy
-        redirect_to test_questions_path(@question.test_id)
-    end
-
-    private
-
-    def find_test
-        @test = Test.find(params[:test_id])
-      end
-
-      def find_question
-        @question = Question.find(params[:id])
-      end
-
-      def question_params
-        params.require(:question).permit(:body)
-      end
-
-      def rescue_with_question_not_found
-        render plain: "Question not found."
-      end
-    end
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :tests do
     resources :questions, shallow: true
   end
+  
 end
