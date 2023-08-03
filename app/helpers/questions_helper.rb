858ec2454c3b03affd7befd0d2ba6e9a7d891fef
Gleb <glebonavt@bk.ru>
2023-08-03 20:29:14 +0300

    4

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,9 +2,9 @@ module QuestionsHelper
 
   def question_header(question)
     if question.new_record?
-       "Edit #{@question.test.title} Question"
+       "Edit #{ @question.test.title } Question"
     else
-       "Create New #{@question.test.title} Question"
+       "Create New #{ @question.test.title } Question"
     end
   end
 
