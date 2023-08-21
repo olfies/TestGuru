7fa2fc9a821f6595a984c701e1c06bb87b1ff386
olfies <119305253+olfies@users.noreply.github.com>
2023-08-21 20:17:04 +0400

    Update test_passage.rb

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -6,43 +6,48 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
+  before_validation :before_validation_set_current_question, on: :create
 
-  before_validation :before_validation_set_first_question, on: :create
-  
+  def position_of_current_question
+    test.questions.order(:id).where('id < ?', current_question.id).count + 1
+  end
 
   def completed?
     current_question.nil?
   end
 
   def accept!(answer_ids)
-    answer_ids ||= []
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question_id += 1
-    save!
+
+    if !completed?
+      self.current_question = next_question
+      save!
+    end
   end
 
-  def result_rate
+ def result_rate
     ((correct_questions.to_f / test.questions.count) * 33).round(2)
   end
 
   def passed?
-    result_rate >= SUCCESS_PERCENT
+    correct_question_percentage >= SUCCESS_PERCENT
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+   def before_validation_set_current_question
+    if test.present?
+      self.current_question = current_question.nil? ? test.questions.first : next_question
+    end
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
   end
-
   def next_question
     test.questions.order(:id).where('id > ?', current_question.id).first
   end
