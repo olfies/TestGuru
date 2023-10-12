e4bcb77f6e1a48c3dddb0e8d54211237089ab6ed
Gleb <glebonavt@bk.ru>
2023-10-12 19:19:08 +0300

    fix flash

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,7 +1,5 @@
 module FlashHelper
-  def flash_message(flash)
-    flash.each do |type, message|
-      content_tag :p, message, class: "flash #{type}"
-    end
+  def flash_message_exists?(key)
+    flash[key].present? if flash
   end
 end
