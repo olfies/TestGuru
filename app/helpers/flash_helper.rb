8f1cc1447f149bb402a4718828aa25c7c15d2ba0
Gleb <glebonavt@bk.ru>
2023-10-12 19:10:58 +0300

    fix 5

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,11 +1,7 @@
 module FlashHelper
-    def flash_message(flash)
-      if flash[:alert]
-        content_tag :p, flash[:alert], class: 'flash alert'
-      else
-        flash.each do |type, message|
-          content_tag :p, message, class: "flash #{type}"
-        end
-      end
+  def flash_message(flash)
+    flash.each do |type, message|
+      content_tag :p, message, class: "flash #{type}"
     end
   end
+end
