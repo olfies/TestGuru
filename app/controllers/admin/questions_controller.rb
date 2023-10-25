5c524117f4305ad72ad45c771ba40b41fd00ce4d
Gleb <glebonavt@bk.ru>
2023-10-25 18:42:57 +0300

    fix 2

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,29 +1,29 @@
-class Admin::QuestionsController < Admin::BaseController
+class Admin::AdminQuestionsController < Admin::BaseController
 
   before_action :find_test, only: %i[new create index]
-  before_action :find_question, only: %i[show destroy edit update]
+  before_action :find_admin_question, only: %i[show destroy edit update]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_admin_question_not_found
 
   def show
   end
 
   def new
-    @question = @test.questions.new
+    @admin_question = @test.questions.new
   end
 
   def create
-    @question = @test.questions.new(question_params)
-    if @question.save
-      redirect_to test_questions_path(@question.test_id)
+    @admin_question = @test.questions.new(admin_question_params)
+    if @admin_question.save
+      redirect_to test_admin_questions_path(@admin_question.test_id)
     else
       render :new
     end
   end
 
   def update
-    if @question.update(question_params)
-      redirect_to test_path(@question.test)
+    if @admin_question.update(admin_question_params)
+      redirect_to test_path(@admin_question.test)
     else
       render :edit
     end
@@ -33,8 +33,8 @@ class Admin::QuestionsController < Admin::BaseController
   end
 
   def destroy
-    @question.destroy
-    redirect_to test_questions_path(@question.test_id)
+    @admin_question.destroy
+    redirect_to test_admin_questions_path(@admin_question.test_id)
   end
 
   private
@@ -43,16 +43,16 @@ class Admin::QuestionsController < Admin::BaseController
     @test = Test.find(params[:test_id])
   end
 
-  def find_question
-    @question = Question.find(params[:id])
+  def find_admin_question
+    @admin_question = Question.find(params[:id])
   end
 
-  def question_params
-    params.require(:question).permit(:body)
+  def admin_question_params
+    params.require(:admin_question).permit(:body)
   end
 
-  def rescue_with_question_not_found
-    render plain: 'Question not found'
+  def rescue_with_admin_question_not_found
+    render plain: 'Admin Question not found'
   end
 
 end
