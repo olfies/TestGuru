109736b4c7d6f56abfc423f1b26d8b023d028bf8
Gleb <glebonavt@bk.ru>
2023-10-31 21:14:20 +0300

    try 1

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -18,7 +18,7 @@ class Admin::TestsController < Admin::BaseController
     @test = current_user.author_tests.build(test_params)
 
     if @test.save
-      redirect_to admin_tests_path
+      redirect_to admin_tests_path, notice: t('.success')
     else
       render :new
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,24 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters
+  before_action :set_locale
 
- protected
 
-  def authenticate_user!
-    unless current_user
-      cookies[:redirect_to] = request.url
-    end
-    cookies[:email] = current_user&.email
+  protected
+
+  def after_sign_in_path_for(resource)
+    resource.admin? ? admin_tests_path : root_path
   end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 
+  def default_url_options
+    {lang: (I18n.locale unless I18n.locale == I18n.default_locale)}
+  end
 
-  def logged_in?
-    current_user.present?
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
